3fcffa634f9be8d1b080a7290e81c116d1c58064
Neel <ratna.neel28@gmail.com>
2025-02-11 18:58:35 +0530

    third version of data saved

--- mycode.py
+++ mycode.py
@@ -10,7 +10,11 @@ df=pd.DataFrame(data)
 
 # # Adding new row to df for V2
 new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
-df.loc[len(df.index)] = new_row_loc                ##df.loc[len(df.index)] ka matlab hai DataFrame ke last index ke baad ek naya row add karna.
+df.loc[len(df.index)] = new_row_loc    ##df.loc[len(df.index)] ka matlab hai DataFrame ke last index ke baad ek naya row add karna.
+
+# # Adding new row to df for V3
+new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+df.loc[len(df.index)] = new_row_loc2
 
 data_dir='data'
 os.makedirs(data_dir, exist_ok=True)
